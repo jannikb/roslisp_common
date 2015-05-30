b61697c45548396efb4da4a764fa6d9a1338a2b8
Jannik Buckelo <jannikbu@tzi.de>
2015-05-30 18:39:29 +0200

    Apparently TinyXml expects a new line at the end of an xml string and s-xml doesnt print that automatically

--- cl_urdf/src/writer.lisp
+++ cl_urdf/src/writer.lisp
@@ -56,7 +56,9 @@ seperated by a whitespcae."
 
 (defun generate-urdf-string (robot)
   "Creates a urdf descriptions as a string of the given `robot'."
-  (s-xml:print-xml-string (generate-urdf-xml robot) :input-type :xml-struct))
+  (s-xml:print-xml-string (generate-urdf-xml robot)
+                          :input-type :xml-struct
+                          :pretty t))
 
 
 ;;; Functions for conversion from a robot part into a xml-element
